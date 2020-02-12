bba8a595cf6ecebf38efcb9fbc83bb6c286d7c18
Patrick Perkins <plp248@nyu.edu>
2020-02-12 16:51:21 -0500

    identifies manuscript page and consecutive page

--- LJS_scraper.rb
+++ LJS_scraper.rb
@@ -14,7 +14,9 @@ def random_manuscript_xml_url
   # and reject any value that is equal to any word in our skip array. finally,
   # take the leftover tags and create an array of the text associated with
   # each tag.
-  hrefs = @ljs_html.css('div#div_directory a').reject { |node| skip.include? node.text }.map(&:text)
+  hrefs = @ljs_html.css('div#div_directory a').reject { |node|
+    skip.include? node.text
+  }.map(&:text)
   random_manuscript = hrefs.sample.gsub('/', '')
   "#{@ljs_url}#{random_manuscript}/data/#{random_manuscript}_TEI.xml"
 end
@@ -24,8 +26,19 @@ def find_matching_images(xml_url)
   manuscript_xml = Nokogiri::XML(open(xml_url)).remove_namespaces!
   # create array of all Nokogiri nodes where the tag is <surface>
   # and the 'n' attribute starts with a number
-  manuscript_xml.xpath('//surface').select { |node| node['n'][0] =~ /\d/ }
-  # iterate through these nodes and choose 2 consecutive images (ie, 4v/5r)
+  page_array = manuscript_xml.xpath('//surface').select {
+      |node| node['n'][0] =~ /\d+/
+  }.to_a
+  # select first random page
+  random_page = nil
+  counter = 0
+  until random_page && random_page['n'] =~ /\d+v/ # any digits followed by a v
+    random_page = page_array.sample
+    counter += 1
+    break if counter > 1000 # to avoid infinite loop
+  end
+  random_page_index = page_array.index(random_page)
+  [page_array[random_page_index], page_array[random_page_index + 1]]
 end
 
 url = random_manuscript_xml_url
