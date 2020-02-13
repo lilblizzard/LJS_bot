0cd434d8eba286e218a5a2c4c26bc942919315c0
Patrick Perkins <plp248@nyu.edu>
2020-02-13 12:32:34 -0500

    refactoring, still buggy

--- LJS_scraper.rb
+++ LJS_scraper.rb
@@ -8,40 +8,51 @@ require 'open-uri'
 
 def random_manuscript_xml_url
   skip = ['Name', 'Last modified', 'Size', 'Parent Directory']
-
-  # take our page html and grab all <a> tags that are inside <div> tags
-  # whose class is equal to div_directory. then, take all of those <a> tags
-  # and reject any value that is equal to any word in our skip array. finally,
-  # take the leftover tags and create an array of the text associated with
-  # each tag.
-  hrefs = @ljs_html.css('div#div_directory a').reject { |node|
-    skip.include? node.text
+  hrefs = @ljs_html.css('div#div_directory a').reject {
+      |node| skip.include? node.text
   }.map(&:text)
   random_manuscript = hrefs.sample.gsub('/', '')
   "#{@ljs_url}#{random_manuscript}/data/#{random_manuscript}_TEI.xml"
 end
 
-def find_matching_images(xml_url)
-  # removing namespaces so that we can xpath - lazy but works
+# TODO - this function can sometimes return nil
+# this screws over other functions that depend
+# on its output to function properly
+def xml_follows_naming_convention(xml_url)
   manuscript_xml = Nokogiri::XML(open(xml_url)).remove_namespaces!
-  # create array of all Nokogiri nodes where the tag is <surface>
-  # and the 'n' attribute starts with a number
   page_array = manuscript_xml.xpath('//surface').select {
-      |node| node['n'][0] =~ /\d+/
+      |node| node['n'] =~ /\d+/
   }.to_a
-  # select first random page
+  if page_array.length >= 2
+    manuscript_xml
+  elsif page_array.nil?
+    puts "#{xml_url} doesn't work."
+  end
+end
+
+def find_matching_images(manuscript_xml)
+  page_array = manuscript_xml.xpath('//surface').to_a
   random_page = nil
   counter = 0
-  until random_page && random_page['n'] =~ /\d+v/ # any digits followed by a v
+  # TODO fix this loop from running infinitely
+  until random_page && random_page['n'] =~ /\d+v/ || (counter == 100)
     random_page = page_array.sample
     counter += 1
-    break if counter > 1000 # to avoid infinite loop
+    puts 'searching...'
   end
   random_page_index = page_array.index(random_page)
   [page_array[random_page_index], page_array[random_page_index + 1]]
 end
 
-url = random_manuscript_xml_url
-puts url
-puts find_matching_images(url)
+# main loop
+image_path_pair = nil
+counter = 0
+until image_path_pair || (counter == 1000)
+  image_path_pair = find_matching_images(xml_follows_naming_convention(random_manuscript_xml_url))
+  counter += 1
+  puts image_path_pair
+end
+
+
+
 
